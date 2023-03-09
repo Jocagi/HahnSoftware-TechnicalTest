f7b569cbd969887448665046e7b9d041c5742246
José Girón <42883411+Jocagi@users.noreply.github.com>
2023-03-08 23:52:18 -0600

    Added filters and buttoms to overview

--- AngularApp/angular-crud/src/app/orders/orders-overview/orders-overview.component.ts
+++ AngularApp/angular-crud/src/app/orders/orders-overview/orders-overview.component.ts
@@ -12,20 +12,49 @@ import { Router } from '@angular/router';
 export class OrdersOverviewComponent implements OnInit {
 
   orders: Order[] = [];
+  filteredOrders: Order[] = [];
+  searchText = '';
 
   constructor(
     private ordersService: OrdersService,
     private router: Router) { }
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(): void {
     var request = this.ordersService.getOrders(AuthService.token).subscribe(orders => {
       this.orders = orders;
+      this.applyFilters();
     });
     console.log('OrdersOverviewComponent.ngOnInit(): request:', request);
   }
 
   logout() {
     this.router.navigateByUrl('/login');
+  }
+
+  applyFilters(): void {
+    this.filteredOrders = this.orders.filter(order => 
+      order.customerName.toLowerCase().includes(this.searchText.toLowerCase()));
+  }
+
+  clearFilters(): void {
+    this.searchText = '';
+    this.applyFilters();
+  }
+
+  editOrder(order: Order): void {
+    // TODO: Implement edit functionality
+  }
+
+  deleteOrder(order: Order): void {
+    // TODO: Implement delete functionality
+  }
+
+  createOrder(): void {
+    // TODO: Implement create functionality
   }
 
 }
